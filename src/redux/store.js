fe0e8a344b5c552beff2c53a496cf192c2e2c7b2
coderyogesh <yogeshrj1433@gmail.com>
2021-06-15 15:57:38 +0530

    updating stripe integration and updating our favicon and title

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,6 +6,12 @@ import rootReducer from './root-reducer';
 
 const middlewares = [logger];
 
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger);
+}
+
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 export const persistor = persistStore(store);
+
+export default { store, persistStore };
